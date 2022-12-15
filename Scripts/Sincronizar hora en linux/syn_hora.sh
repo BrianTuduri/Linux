15d03c198adf8e1e01bf77de1bfe700a6debf7f2
Brian Tuduri <106203739+BrianTuduri@users.noreply.github.com>
2022-12-15 09:35:17 -0300

    Contador de tareas

--- Scripts/Sincronizar hora en linux/syn_hora.sh
+++ Scripts/Sincronizar hora en linux/syn_hora.sh
@@ -2,6 +2,23 @@
 
 function sincronizar {
 
+  # Variables.
+  let proc=5
+  s_null="/dev/null"
+  RED=$(tput setaf 1)
+  NORMAL=$(tput sgr0)
+  GREEN=$(tput setaf 2)
+
+  # Indicamos al usuario que comenzamos las tareas.
+  echo
+  echo "[Realizando tareas, espere un momento por favor (No cierre la terminal)...]"
+  echo
+  printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+  echo
+
+  # Tareas.
+  #
+
   echo "================================================================================"
   echo "      Comenzando la comprobación del paquete chronyc "
   echo "                   Por favor no cierre la terminal..."
@@ -27,26 +44,30 @@ function sincronizar {
     fi
   fi
 
+  let "proc -= 1"
+  echo
+  printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+  echo
+
+  #
   # Obtenemos la ip por parametro
 
   echo
   echo "================================================================================"
-  echo "      Comenzando la configuracion de chronyc, debera introducir la ip "
+  echo "      Comenzando la configuración de chronyc, debera introducir la ip "
   echo "                   Por favor no cierre la terminal..."
   echo "================================================================================"
   echo
-  echo "Introduce la dirección IP del servidor NTP:"
-  read IP
 
   regex="^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$"
   i=1
 
-  while [ $i -ne 0 ]; do
-
+  while [ $i != 0 ]; do
+    echo "Introduce la dirección IP del servidor NTP:"
+    read IP
     # Comprobamos si la dirección IP cumple con el formato
     if [[ $IP =~ $regex ]]; then
       if [[ ${BASH_REMATCH[1]} -le 255 && ${BASH_REMATCH[2]} -le 255 && ${BASH_REMATCH[3]} -le 255 && ${BASH_REMATCH[4]} -le 255 ]]; then
-        echo "La dirección IP es válida"
         #La dirección IP es valida
         if [ -z "$IP" ]; then
 
@@ -65,29 +86,65 @@ function sincronizar {
 
             # Si no se encuentra, agrega la linea de configuracion
             echo "$cadena" >>"$archivo"
-            echo "No se encontro la linea de configuración, se procede a crearla"
+            echo
+            echo "No se encontro la linea de configuración, se procede a crearla."
+            echo
+            let "proc -= 1"
+            #printf "%40s\n" "${GREEN}Procesos restantes: ${NORMAL}"$proc
+            echo
+            printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+            echo
+
+            #
           else
 
             # Si se encuentra la modifica
+            echo
             echo "Se encontro configurado: $(grep -F "$cadena" "$archivo")"
+            echo
             sudo sed -i "s/server.*/server $IP prefer iburst minpoll 4 maxpoll 4/g" $archivo
-            echo "Se aplico correctamente la configuración para la IP:$IP."
+            echo "Se aplico correctamente la configuración para la IP:$IP"
+            let "proc -= 1"
+            echo
+            printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+            echo
+
+            #
           fi
 
           # Reiniciar chrony para aplicar la configuración
+          echo
           echo "Reiniciando el servicio de chronyd..."
+          echo
           sudo systemctl restart chronyd
-          echo "Reinicio terminado"
+          echo
+          echo "Reinicio terminado."
+          echo
+          let "proc -= 1"
+          echo
+          printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+          echo
+
+          #
 
           # Comprobar si se sincronizó correctamente con el servidor de tiempo
+          echo
           echo "Comprobacion de sincronización..."
+          echo
           chronyc sources -v
+          let "proc -= 1"
+          echo
+          printf " - ${GREEN}Procesos restantes: ${NORMAL}"$proc
+          echo
 
-        #if de verificacion ip
+          #
+
+          #if de verificacion ip
+          #
+          i=0
+          break
         fi
-        #
-        i=0
-        break
+
       else
         printf "Error: %s\n" "la dirección ip excede el rango." 1>&2
         i=$((i + 1))
@@ -101,6 +158,13 @@ function sincronizar {
     fi
   done
 
+  echo
+  printf "${GREEN}[Tareas finalizadas con éxito]${NORMAL}"
+
+  echo
+  # Liberamos Variables.
+  proc=
+  s_null=
 }
 
 if [ "$(id -u)" != "0" ]; then
